4a7026a4a72e9b6acf7fcfc6971d9ade36b84fb9
Kayo Weiber <caioveiber598@gmail.com>
2024-12-11 17:19:07 -0300

    tentativa

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import login from './Components/login/login'
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <login />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/login/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const login = () => {
+    return (
+
+        <div className="container">
+            <form>
+                <h1> Acesse o sistema</h1>
+                <input type="email" placeholder='E-mail' />
+
+            </form>
+        </div>
+
+    )
+}
+
+export default login
