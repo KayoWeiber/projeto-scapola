28f130af469d43326218960b73e8f9e51a235214
Kayo Weiber <caioveiber598@gmail.com>
2024-12-12 20:18:50 -0300

    att
    Mudança na Alteração do background-color

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -1,4 +1,4 @@
-import {FaLock, FaUser} from "react-icons/fa"
+import {FaLock, FaUser,FaEyeSlash} from "react-icons/fa"
 
 import { useState } from "react"
 import "./login.css"
@@ -11,18 +11,20 @@ const Login = () => {
         console.log(username,password)
         console.log("Envio")
     }
+
     return (
 
         <div className="container">
             <form onSubmit={handleSubmit}>
                 <h1> Acesse o sistema</h1>
                 <div className="input-field">
-                    <input type="text" placeholder='Usuário' onChange={(e)=>setUsername(e.target.value)} required/>
+                    <input type="text" placeholder='Usuário' onChange={(e)=>setUsername(e.target.value)} />
                     <FaUser className="icon"/>
                 </div>
                 <div className="input-field">
-                    <input type="password" placeholder='Senha'onChange={(e)=>setPassword(e.target.value)} required/>
+                    <input type="password" placeholder='Senha'onChange={(e)=>setPassword(e.target.value)}/>
                     <FaLock className="icon"/>
+                   {/* <FaEyeSlash className="icon"/> */}
                 </div>
                 <div className="recall-forget">
                     <label htmlFor="">
