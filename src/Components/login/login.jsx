2d057ade0269f43ca2be65862abc037f0379aea7
Kayo Weiber <caioveiber598@gmail.com>
2024-12-12 21:56:14 -0300

    Mudanças-componentes
    alteração de alguns componentes

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -1,4 +1,4 @@
-import {FaLock, FaUser,FaEyeSlash} from "react-icons/fa"
+import {FaLock, FaUser,FaEyeSlash,FaRegEye } from "react-icons/fa"
 
 import { useState } from "react"
 import "./login.css"
@@ -6,11 +6,13 @@ import "./login.css"
 const Login = () => {
     const [username,setUsername]=useState("")
     const [password,setPassword]=useState("")
+    const [showPassword, setShowPassword] = useState(false);
     const handleSubmit=(e) =>{
         e.preventDefault()
         console.log(username,password)
         console.log("Envio")
     }
+    const togglePasswordVisibility = () => { setShowPassword(!showPassword);}
 
     return (
 
@@ -18,13 +20,20 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <h1> Acesse o sistema</h1>
                 <div className="input-field">
-                    <input type="text" placeholder='Usuário' onChange={(e)=>setUsername(e.target.value)} />
-                    <FaUser className="icon"/>
+                    <input type="text" placeholder='' onChange={(e)=>setUsername(e.target.value)} />
+                    <label htmlFor="username" className="placeholder-label">Usuário</label>
+                    <FaUser className="icon2"/>
                 </div>
                 <div className="input-field">
-                    <input type="password" placeholder='Senha'onChange={(e)=>setPassword(e.target.value)}/>
-                    <FaLock className="icon"/>
-                   {/* <FaEyeSlash className="icon"/> */}
+                <input 
+                    type={showPassword ? 'text' : 'password'} 
+                    placeholder='' 
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
+                <label htmlFor="password" className="placeholder-label">Senha</label>
+                <span onClick={togglePasswordVisibility} className="icon">
+                {showPassword ? <FaRegEye /> : (password.length > 0 ? <FaEyeSlash /> : <FaLock />)}
+                </span>
                 </div>
                 <div className="recall-forget">
                     <label htmlFor="">
