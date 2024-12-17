7d6a2e35c37085c76a425a5c0d0271ec42d13539
Kayo Weiber <caioveiber598@gmail.com>
2024-12-17 17:30:15 -0300

    alteração do lembre de mim e ajuste no plaholder

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -1,51 +1,51 @@
-import {FaLock, FaUser,FaEyeSlash,FaRegEye } from "react-icons/fa"
+import { FaLock, FaUser, FaEyeSlash, FaRegEye } from "react-icons/fa"
 import logo from "../../assets/logoBranca.svg";
 import { useState } from "react"
-import Lines from "../lines";
+import Lines from "../lines/index";
 import "./login.css"
 
 
 const Login = () => {
-    const [username,setUsername]=useState("")
-    const [password,setPassword]=useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const [showPassword, setShowPassword] = useState(false);
-    const handleSubmit=(e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(username,password)
+        console.log(email, password)
         console.log("Envio")
     }
-    const togglePasswordVisibility = () => { setShowPassword(!showPassword);}
+    const togglePasswordVisibility = () => { setShowPassword(!showPassword); }
 
     return (
         <div className="login-wrapper">
-             <Lines />
+            <Lines />
             <div className="container">
-                
                 <form onSubmit={handleSubmit}>
-
                     <img src={logo} alt="Logo do sistema" className="logo" />
                     <h1> Acesse o sistema</h1>
                     <div className="input-field">
-                        <input type="text" placeholder='' onChange={(e)=>setUsername(e.target.value)} />
-                        <label htmlFor="username" className="placeholder-label">Usuário</label>
-                        <FaUser className="icon2"/>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder=""
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label htmlFor="email" className="placeholder-label">E-mail</label>
+                        <FaUser className="icon2" />
                     </div>
                     <div className="input-field">
-                    <input 
-                        type={showPassword ? 'text' : 'password'} 
-                        placeholder='' 
-                        onChange={(e) => setPassword(e.target.value)} 
-                    />
-                    <label htmlFor="password" className="placeholder-label">Senha</label>
-                    <span onClick={togglePasswordVisibility} className="icon">
-                    {showPassword ? <FaRegEye /> : (password.length > 0 ? <FaEyeSlash /> : <FaLock />)}
-                    </span>
+                        <input
+                            id="password"
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder=""
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label htmlFor="password" className="placeholder-label">Senha</label>
+                        <span onClick={togglePasswordVisibility} className="icon">
+                            {showPassword ? <FaRegEye /> : (password.length > 0 ? <FaEyeSlash /> : <FaLock />)}
+                        </span>
                     </div>
                     <div className="recall-forget">
-                        <label htmlFor="">
-                            <input type="checkbox"/>
-                            Lembre de mim
-                        </label>
                         <a href="a">Esqueceu a senha?</a>
                     </div>
                     <button>Entrar</button>
