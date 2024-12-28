f2d881e7d0ce6d2f7d6dcaa5026f106082e3812e
Kayo Weiber <caioveiber598@gmail.com>
2024-12-28 18:50:48 -0300

    ajustes main

--- server/index.js
+++ server/index.js
@@ -27,9 +27,9 @@ app.post('/login', async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {return res.status(400).json({ error: 'E-mail e senha são obrigatórios' });}
     try {
-        const result = await pool.query('SELECT * FROM login WHERE email = $1', [email]);
-        if (result.rows.length === 0) {
-            return res.status(404).json({ error: 'Usuário não encontrado', type: 'not-found' });
+        const result = await pool.query('SELECT * FROM login WHERE email = $1',[email]);
+        if (result.rows.length===0) {
+            return res.status(404).json({ error:'Usuário não encontrado', type: 'not-found' });
         }
         const user = result.rows[0];
         const validPassword = await bcrypt.compare(password, user.senha);
--- server/teste.js
+++ server/teste.js
@@ -1,11 +1,10 @@
-const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt';
 
-const senhaOriginal = '123456'; // Senha que você deseja testar
+const senhaOriginal = '123456'; // Senha que para hash
 bcrypt.hash(senhaOriginal, 10, (err, hash) => {
     if (err) {
         console.error('Erro ao gerar hash:', err);
     } else {
         console.log('Hash gerado:', hash);
-        // Atualize este hash no banco de dados para o usuário desejado
     }
 });
